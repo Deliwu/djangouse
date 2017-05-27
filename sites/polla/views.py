63a1d9fabd3bd9023b655b24c31875ea917a8041
testa <testa@qq.com>
2017-05-27 15:54:34 +0800

    Model 操作

--- sites/polla/views.py
+++ sites/polla/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Qustion
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h2>All polla index</h2>")
+    latest_question_list = Qustion.objects.order_by('-pub_date')[:5]
+    output = ','.join([question.question_text for question in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse('Your are looking at question {}'.format(question_id))
+
+
+def results(request, question_id):
+    response = "You are looking at results of question {}".format(question_id)
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse("Your are voting on question {}".format(question_id))
