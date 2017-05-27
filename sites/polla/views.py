042173e18e9e530e4b38e34a403bee95e99ca6b6
testa <testa@qq.com>
2017-05-27 17:38:45 +0800

    新增static,静态文件

--- sites/polla/views.py
+++ sites/polla/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Qustion
+from django.template import loader
 
 
 # Create your views here.
 def index(request):
     latest_question_list = Qustion.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polla/index.html')
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'polla/index.html', context)
+    return HttpResponse(template.render(context))
 
 
 def detail(request, question_id):
